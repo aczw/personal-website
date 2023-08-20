1eaafc382b2bcd085272c8bd36f815cce41b1a7c
Charles Wang <czw@seas.upenn.edu>
2023-08-20 02:35:07 -0700

    restart everything

--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  printWidth: 100,
-  singleAttributePerLine: true,
-};
-
-module.exports = config;
