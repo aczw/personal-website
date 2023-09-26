903e482203f6aa75b49455bb2592d2bac538aef6
Charles Wang <czw@seas.upenn.edu>
2023-09-26 01:15:24 -0400

    add clsx and tailwind-merge

--- src/components/react/link.tsx
+++ src/components/react/link.tsx
@@ -1,17 +1,24 @@
+import { cn } from "@/utils/cn";
+
 const Link = ({
   href,
   newTab = false,
+  className,
   children,
 }: {
   href: string;
   newTab?: boolean;
+  className?: string;
   children: React.ReactNode;
 }) => {
   return (
     <a
       href={href}
       target={newTab ? "_blank" : "_self"}
-      className="w-fit text-sweater-3 underline decoration-sweater-3/50 underline-offset-2 transition-colors hover:text-sweater-1 hover:decoration-sweater-3"
+      className={cn(
+        "w-fit text-sweater-3 underline decoration-sweater-3/50 underline-offset-2 transition-colors hover:text-sweater-1 hover:decoration-sweater-3",
+        className,
+      )}
     >
       {children}
     </a>
