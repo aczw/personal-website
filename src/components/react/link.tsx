1d784e9205700d6706bae15ece2ac39a414c8c5c
Charles Wang <czw@seas.upenn.edu>
2023-09-28 02:29:43 -0400

    a11y: add titles to links

--- src/components/react/link.tsx
+++ src/components/react/link.tsx
@@ -2,11 +2,13 @@ import { cn } from "@/utils/cn";
 
 const Link = ({
   href,
+  title,
   newTab = false,
   className,
   children,
 }: {
   href: string;
+  title?: string;
   newTab?: boolean;
   className?: string;
   children: React.ReactNode;
@@ -15,6 +17,8 @@ const Link = ({
     <a
       href={href}
       target={newTab ? "_blank" : "_self"}
+      title={title}
+      aria-label={title}
       className={cn(
         "w-fit text-sweater-3 underline decoration-sweater-3/50 underline-offset-2 transition-all hover:text-sweater-1 hover:decoration-sweater-1",
         className,
