408af77cceb001abc188aa2250912b2efc9d3b24
Charles Wang <czw@seas.upenn.edu>
2023-10-08 22:55:57 -0400

    FIGURE OUT ANIMATIONS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,13 @@ const config = {
         ...defaultTheme.screens,
         xs: "475px",
       },
+      keyframes: {
+        ...defaultTheme.keyframes,
+        slide: {
+          "0%": { transform: "translateY(15px)", opacity: "0" },
+          "100%": { transform: "translateY(0px)", opacity: "1" },
+        },
+      },
     },
   },
   plugins: [],
