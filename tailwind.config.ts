9f9f71eb9ee01606f5161efd3b0a8779cb1f503b
Charles Wang <czw@seas.upenn.edu>
2023-10-25 19:00:17 -0400

    delete glass blur plugin since I'm not using it...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import { type Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
-import plugin from "tailwindcss/plugin";
 
 const config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -43,71 +42,7 @@ const config = {
       },
     },
   },
-  // thank you! https://twitter.com/lepikhinb/status/1707727096083255330
-  plugins: [
-    plugin(({ matchUtilities, theme }) => {
-      matchUtilities(
-        {
-          glass: (
-            value: string,
-            { modifier }: { modifier: string | number | null },
-          ) => {
-            const offset = modifier ?? value;
-            const height = `calc(100% - ${offset})`;
-
-            return {
-              "&::before": {
-                content: "var(--tw-content)",
-                position: "absolute",
-                inset: "0",
-                bottom: `-${offset}`,
-                maskImage: `linear-gradient(to bottom, black 0, black ${height}, transparent ${height})`,
-                "--tw-backdrop-blur": `blur(${value})`,
-                backdropFilter:
-                  "var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate)",
-              },
-            };
-          },
-        },
-        {
-          values: theme("blur"),
-          modifiers: theme("spacing"),
-        },
-      );
-
-      matchUtilities(
-        {
-          "glass-edge": (
-            value: string,
-            { modifier }: { modifier: string | number | null },
-          ) => {
-            const offset = modifier ?? value;
-            const top = `calc(100% - ${offset} - 1px)`;
-            const bottom = `calc(100% - ${offset})`;
-
-            return {
-              "&::before": {
-                content: "var(--tw-content)",
-                position: "absolute",
-                inset: "0",
-                bottom: `-${offset}`,
-                maskImage: `linear-gradient(to bottom, transparent 0, transparent ${top}, black ${top}, black ${bottom}, transparent ${bottom})`,
-                "--tw-backdrop-blur": `blur(${value})`,
-                "--tw-backdrop-brightness": `brightness(1.5)`,
-                "--tw-backdrop-saturate": `saturate(1.5)`,
-                backdropFilter:
-                  "var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate)",
-              },
-            };
-          },
-        },
-        {
-          values: theme("blur"),
-          modifiers: theme("spacing"),
-        },
-      );
-    }),
-  ],
+  plugins: [],
 } satisfies Config;
 
 export default config;
