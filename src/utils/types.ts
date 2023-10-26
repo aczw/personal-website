a4274a91bda2f8e79a413cce28bf12b1a3ea49bf
Charles Wang <czw@seas.upenn.edu>
2023-10-25 22:34:27 -0400

    revamp portfolio page

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,12 @@
 import type portfolioJson from "@/assets/portfolio/portfolio.json";
 import type projectsJson from "@/assets/projects/projects.json";
 
+// TS currently interprets enums as just strings, so we manually override it
+// see https://github.com/microsoft/TypeScript/issues/32063
+type Portfolio = Omit<(typeof portfolioJson)[0], "type"> & {
+  type: "design" | "art" | "music";
+};
+
 type Project = (typeof projectsJson)[0];
-type Portfolio = (typeof portfolioJson)[0];
 
 export type { Portfolio, Project };
