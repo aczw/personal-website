306f920291a7e5ace173947c6f9de80b67a03f7a
Charles Wang <czw@seas.upenn.edu>
2023-11-12 23:19:10 -0500

    correct prettier behavior

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
-/** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
+/** @type {import("prettier-plugin-tailwindcss").PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  plugins: [ "prettier-plugin-tailwindcss" ],
   singleAttributePerLine: true,
-  tailwindFunctions: ["cn", "clsx", "twMerge"],
+  tailwindFunctions: [ "cn", "clsx", "twMerge" ],
 };
 
 export default config;
