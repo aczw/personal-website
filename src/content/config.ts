a400c2786f6d937b168447bc87da5f698a7c55cc
Charles Wang <czw@seas.upenn.edu>
2023-12-18 01:48:09 -0500

    add some more metadata

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,14 +9,19 @@ const projectsCollection = defineCollection({
       text: z.string()
     }).optional(),
     blurb: z.string(),
-    date: z.date(),
+    duration: z.object({
+      date: z.date(),
+      length: z.string(),
+    }),
     cover: z
       .object({
         img: image(),
         alt: z.string(),
       }),
     tags: z.array(z.string()),
-    order: z.number().min(0),
+    role: z.array(z.string()),
+    teamSize: z.number().min(2).optional(),
+    order: z.number().min(1),
     published: z.boolean()
   }),
 });
