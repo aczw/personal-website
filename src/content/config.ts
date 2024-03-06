ed12f7217d3d26b2bc64cdd8b83dcd4fa80d53f3
Charles Wang <czw@seas.upenn.edu>
2024-03-06 15:36:58 -0500

    Add new fields

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,50 +2,57 @@ import { defineCollection, z } from "astro:content";
 
 const writeUps = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    link: z.object({
-      url: z.string().url(),
-      text: z.string()
-    }).optional(),
-    blurb: z.string(),
-    duration: z.object({
-      date: z.date(),
-      length: z.string(),
-    }),
-    cover: z
-      .object({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      link: z
+        .object({
+          url: z.string().url(),
+          text: z.string(),
+        })
+        .optional(),
+      blurb: z.string(),
+      duration: z.object({
+        from: z.date(),
+        to: z.date(),
+        length: z.string(),
+      }),
+      cover: z.object({
         img: image(),
         alt: z.string(),
       }),
-    tags: z.array(z.string()),
-    role: z.array(z.string()),
-    teamSize: z.number().min(2).optional(),
-    order: z.number().min(1),
-    published: z.boolean()
-  }),
+      tags: z.array(z.string()),
+      role: z.array(z.string()),
+      teamSize: z.number().min(2).optional(),
+      order: z.number().min(1),
+      published: z.boolean(),
+    }),
 });
 
 const projects = defineCollection({
   type: "data",
   schema: z.object({
     title: z.string(),
-    link: z.object({
-      url: z.string().url(),
-      text: z.string()
-    }).optional(),
+    link: z
+      .object({
+        url: z.string().url(),
+        text: z.string(),
+      })
+      .optional(),
     blurb: z.string(),
     date: z.coerce.date(),
-    imgs: z.array(z.object({
-      src: z.string(),
-      alt: z.string()
-    })),
+    imgs: z.array(
+      z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+    ),
     tags: z.array(z.string()),
-    order: z.number().min(1)
-  })
-})
+    order: z.number().min(1),
+  }),
+});
 
 export const collections = {
   "write-ups": writeUps,
-  projects
+  projects,
 };
