d830237fb29a615b9789640f4db8eab8b5614b36
Charles Wang <czw@seas.upenn.edu>
2024-05-30 00:51:06 -0700

    v4.4.0
    - cool text animation for mewo quote
    - reorganize posts page
    - add dates to posts
    - remove post tags
    - make images optional for posts
    - cool animation when hovering over a post

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,26 +8,29 @@ const posts = defineCollection({
       blurb: z.string().refine((blurb) => blurb.length <= 190, {
         message: "Blurb should be 190 characters or less, so it fits on the home screen.",
       }),
-      tags: z.array(z.string()),
-      cover: z.object({
-        img: image().refine(
-          (img) => {
-            const ratio = img.width / img.height;
-            const epsilon = 0.01;
+      posted: z.date(),
+      updated: z.date().optional(),
+      cover: z
+        .object({
+          img: image().refine(
+            (img) => {
+              const ratio = img.width / img.height;
+              const epsilon = 0.01;
 
-            // sometimes we can't get an exact aspect ratio, and that's okay within epsilon
-            if (ratio >= 1.6 - epsilon && ratio <= 1.6 + epsilon) {
-              return true;
-            }
+              // sometimes we can't get an exact aspect ratio, and that's okay within epsilon
+              if (ratio >= 1.6 - epsilon && ratio <= 1.6 + epsilon) {
+                return true;
+              }
 
-            return false;
-          },
-          {
-            message: "Cover image must have a 16:10 aspect ratio (my laptop display)",
-          },
-        ),
-        alt: z.string(),
-      }),
+              return false;
+            },
+            {
+              message: "Cover image must have a 16:10 aspect ratio (my laptop display)",
+            },
+          ),
+          alt: z.string(),
+        })
+        .optional(),
 
       published: z.boolean(),
     }),
