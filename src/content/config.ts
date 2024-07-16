adfc5f85eef162c64dbd0bb2fac84ffe565d5117
Charles Wang <charles.wang@bytedance.com>
2024-07-15 17:55:52 -0700

    feat(posts/changelogs): more work

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const changelog = defineCollection({
   type: "content",
   schema: z.object({
       version: z.string().regex(/^v[0-9]+\.[0-9]+\.[0-9]+$/g, {
-        message: "Version numbers should follow semantic versioning!",
+        message: "Version numbers should follow X.Y.Z format!",
       }),
       date: z.date(),
       commit: z.string().length(40).optional(),
