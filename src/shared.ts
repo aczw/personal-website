ee4c867d514d6a0a4e021765c2fc1704a4b891f0
Charles Wang <zwcharl@gmail.com>
2024-12-03 22:57:01 -0500

    reorder /projects, disable videos for some overs
    because running all 10 videos at once is destroying everyone's computers

--- src/shared.ts
+++ src/shared.ts
@@ -12,39 +12,53 @@ type MetaKind =
 class ProjectCover extends HTMLElement {
   externallyPaused: boolean;
 
-  #video: HTMLVideoElement;
-  #wrapper: HTMLDivElement;
+  #hasVideo: boolean;
+  #video: HTMLVideoElement | undefined;
+  #wrapper: HTMLDivElement | undefined;
 
   constructor() {
     super();
 
     this.externallyPaused = false;
-    this.#video = this.querySelector("video")!;
-    this.#wrapper = this.querySelector("div")!;
+
+    // Note: data-* values automatically become camel case, so the key is
+    // actually "useVideo" not "use-video"
+    this.#hasVideo = this.dataset["useVideo"] === "true";
+
+    if (this.#hasVideo) {
+      this.#video = this.querySelector("video")!;
+      this.#wrapper = this.querySelector("div")!;
+    }
   }
 
   connectedCallback() {
+    if (!this.#hasVideo) return;
+
     // Unless the video is already able to be played to the end without buffering, we attach
     // an event listener that displays and plays the video only when it's able to
-    if (this.#video.readyState !== HTMLMediaElement.HAVE_ENOUGH_DATA) {
-      this.#video.addEventListener("canplaythrough", () => this.playVideo());
+    if (this.#video!.readyState !== HTMLMediaElement.HAVE_ENOUGH_DATA) {
+      this.#video!.addEventListener("canplaythrough", () => this.playVideo());
     } else {
       this.playVideo();
     }
   }
 
   playVideo() {
+    if (!this.#hasVideo) return;
+
     // User may have chosen to pause all videos on the page externally using the
     // ProjectCoverControls button. We respect that choice here and don't play this video
     if (this.externallyPaused) return;
 
-    this.#video.play();
-    this.#wrapper.style.opacity = "100";
+    this.#video!.play();
+    this.#wrapper!.style.opacity = "100";
   }
 
   pauseVideo() {
-    this.#video.pause();
-    this.#wrapper.style.opacity = "0";
+    if (!this.#hasVideo) return;
+
+    this.#video!.pause();
+    this.#wrapper!.style.opacity = "0";
   }
 }
 
