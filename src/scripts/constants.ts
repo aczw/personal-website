01439b2f87441440cab710bd2851c0ef5759f27c
Charles Wang <zwcharl@gmail.com>
2025-12-22 01:14:10 -0500

    begin /

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,12 +1,7 @@
-import {
-  FileUserIcon,
-  LinkedinIcon,
-  GithubIcon,
-  MailIcon,
-} from "@lucide/astro";
-
 const CDN_URL = "https://cdn.charleszw.com";
+
 const PROJECT_CATEGORIES = ["graphics", "games", "art"] as const;
+
 const VALID_MONTHS = [
   "January",
   "February",
@@ -24,23 +19,4 @@ const VALID_MONTHS = [
 
 const DISABLED_PROJECTS = ["dgdg", "deth"];
 
-function getSocials() {
-  return [
-    { href: "/_files/resume.pdf", label: "Resume", icon: FileUserIcon },
-    {
-      href: "https://linkedin.com/in/zwcharl",
-      label: "LinkedIn",
-      icon: LinkedinIcon,
-    },
-    { href: "https://github.com/aczw", label: "GitHub", icon: GithubIcon },
-    { href: "mailto:zwcharl@gmail.com", label: "Email", icon: MailIcon },
-  ];
-}
-
-export {
-  CDN_URL,
-  PROJECT_CATEGORIES,
-  VALID_MONTHS,
-  DISABLED_PROJECTS,
-  getSocials,
-};
+export { CDN_URL, PROJECT_CATEGORIES, VALID_MONTHS, DISABLED_PROJECTS };
