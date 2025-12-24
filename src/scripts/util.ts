0401a6304cbb6d3615eecabfd0cdd3dc1c846023
Charles Wang <zwcharl@gmail.com>
2025-12-24 00:38:04 -0500

    progress bar

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -1,7 +1,3 @@
-import { readdirSync } from "node:fs";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
-
 import type { ContentDateType, DateKind } from "@/scripts/types";
 
 /**
@@ -10,36 +6,6 @@ import type { ContentDateType, DateKind } from "@/scripts/types";
  * e.g. constants.ts. Therefore it's defined in this file instead.
  */
 const SITE_URL = "https://charleszw.com";
-
-/**
- * Astro's sitemap integration does not include dynamic routes in the generated sitemap when
- * the site uses SSR. This function crawls the filesystem and does it manually.
- *
- * Note: this also includes routes that are private/drafts, because it does not check frontmatter.
- *
- * Note: currently unused because projects and posts are statically generated since v5.0.0. Check
- * file paths before using.
- *
- * @see https://github.com/withastro/astro/issues/3682#issuecomment-1492468918
- */
-function getContentRoutes() {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-
-  const contentPath = join(__dirname, "src", "content");
-
-  const postUrls = readdirSync(join(contentPath, "posts")).map((file) => {
-    const fileName = file.split(".")[0];
-    return `${SITE_URL}/posts/${fileName}`;
-  });
-
-  const projectUrls = readdirSync(join(contentPath, "projects")).map((file) => {
-    const fileName = file.split(".")[0];
-    return `${SITE_URL}/projects/${fileName}`;
-  });
-
-  return [...postUrls, ...projectUrls];
-}
 
 /**
  * Checks that the cover image for my project covers have an aspect ratio of
@@ -108,13 +74,17 @@ function getDateKind(date: ContentDateType): DateKind {
   }
 }
 
+function saturate(value: number) {
+  return Math.max(Math.min(value, 1), 0);
+}
+
 export {
   SITE_URL,
-  getContentRoutes,
   validProjectCover,
   stripEndingSlash,
   getShortDateFormatting,
   getFullDateFormatting,
   getMonthYearDateFormatting,
   getDateKind,
+  saturate,
 };
