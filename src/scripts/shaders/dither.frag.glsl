798c9346e02c411b2833cfb1d46e73e93c2649b2
Charles Wang <zwcharl@gmail.com>
2025-12-24 23:25:18 -0500

    webgl: noise-based dithering

--- src/scripts/shaders/dither.frag.glsl
+++ src/scripts/shaders/dither.frag.glsl
@@ -7,9 +7,30 @@ in vec2 f_uv;
 
 out vec4 out_color;
 
-void main() {
-  vec2 flipped_uv = vec2(f_uv.x, 1.0f - f_uv.y);
-  vec4 video_color = texture(u_video_frame, flipped_uv);
+/// See https://stackoverflow.com/questions/12964279/whats-the-origin-of-this-glsl-rand-one-liner
+float random(vec2 x) {
+  return fract(sin(dot(x, vec2(12.9898f, 78.233f))) * 42758.5453f);
+}
 
-  out_color = vec4(1.0f - video_color.rgb, 1.0f);
+/// https://blog.maximeheckel.com/posts/the-art-of-dithering-and-retro-shading-web/#a-first-pass-at-dithering-in-react-three-fiber
+vec3 noise_dither(vec2 uv, float luminance) {
+  if (luminance < random(uv)) {
+    return vec3(0.0f);
+  } else {
+    return vec3(1.0f);
+  }
+}
+
+/// See https://en.wikipedia.org/wiki/Grayscale#Converting_color_to_grayscale
+float to_luminance(vec3 color) {
+  return dot(vec3(0.2126f, 0.7152f, 0.0722f), color);
+}
+
+void main() {
+  vec2 uv = vec2(f_uv.x, 1.0f - f_uv.y);
+  vec4 video_color = texture(u_video_frame, uv);
+  float luminance = to_luminance(video_color.rgb);
+  vec3 final_color = noise_dither(uv, luminance);
+
+  out_color = vec4(final_color, 1.0f);
 }
