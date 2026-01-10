26ba091ab8755de6554a4b8356c7a4a2dc1a00df
Charles Wang <zwcharl@gmail.com>
2026-01-10 07:43:50 -0500

    add some metadata

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -11,7 +11,7 @@ const SITE_URL = "https://charleszw.com";
  * Checks that the cover image for my project covers have an aspect ratio of
  * 16:10, for no real reason other than consistency and aesthetics
  */
-function validProjectCover(width: number, height: number): boolean {
+function isValidProjectCover(width: number, height: number): boolean {
   const ratio = width / height;
 
   if (Math.abs(ratio - 1.6) <= 0.01) {
@@ -80,7 +80,7 @@ function saturate(value: number) {
 
 export {
   SITE_URL,
-  validProjectCover,
+  isValidProjectCover,
   stripEndingSlash,
   getShortDateFormatting,
   getFullDateFormatting,
