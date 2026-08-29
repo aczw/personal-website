c03b8be7efe43108952e623b8e4042dd9bc80a6f
Charles Wang <git@fwd.czw.sh>
2026-08-28 23:48:15 -0700

    draw loop, quad vert shader

--- src/shaders/no-op.vert.glsl
+++ src/shaders/no-op.vert.glsl
@@ -1,11 +1,16 @@
 #version 300 es
 
-in vec4 v_position;
-in vec2 v_uv;
+out vec2 frag_uv;
 
-out vec2 f_uv;
+// Coordinates for drawing an oversized triangle (acts as fullscreen quad)
+const vec2 UV_COORDS[] = vec2[](vec2(0.f, 0.f), vec2(2.f, 0.f), vec2(0.f, 2.f));
 
 void main() {
-  f_uv = v_uv;
-  gl_Position = v_position;
+  vec2 uv = UV_COORDS[gl_VertexID];
+  // Derive clip space positions from UV coords. This gives us
+  // [0] = (-1f, -1.f), [1] = (3.f, -1.f), [2] = (-1.f, 3.f)
+  vec2 position_xy = (2.f * uv) - 1.f;
+
+  frag_uv = uv;
+  gl_Position = vec4(position_xy, 0.f, 1.f);
 }
--- src/shaders/red.frag.glsl
+++ src/shaders/red.frag.glsl
@@ -1,8 +1,10 @@
 #version 300 es
 precision highp float;
 
+in vec2 frag_uv;
+
 out vec4 out_color;
 
 void main() {
-  out_color = vec4(1.f, 0.f, 0.f, 1.f);
+  out_color = vec4(frag_uv, 0.f, 1.f);
 }
